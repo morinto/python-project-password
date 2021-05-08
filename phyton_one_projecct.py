bb4b658a1e41cf53d7451dc911389398cf20d84c
morinto <egbe.ufuoma@gmail.com>
2021-05-08 06:33:06 -0700

    a welcome password statement

---
+++ phyton_one_projecct.py
@@ -0,0 +1,14 @@
+username = input("Enter username: ")
+
+if username == "admin":
+    pwd = input("enter password: ")
+    if pwd == "password":
+        print("Welcome")
+    elif pwd == "":
+        print("Cancelled")
+    else:
+        print("Wrong password")
+elif username == "":
+    print("Cancelled")
+else:
+    print("You are not a member")
